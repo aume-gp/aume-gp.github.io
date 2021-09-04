1da565b1ea9857688e553712f4ac1ac5b4de8b15
aume-gp <77577837+aume-gp@users.noreply.github.com>
2021-09-04 19:00:10 +0200

    Delete coordinates.js

--- js/coordinates.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('mousemove', function (e) {
-    document.getElementById('x-value').textContent = e.x + ",00";
-    document.getElementById('y-value').textContent = e.y + ",00";
-  });
